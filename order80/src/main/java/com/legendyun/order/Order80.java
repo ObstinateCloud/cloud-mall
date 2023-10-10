2ff819f5e4e824ea071191f539fd309a7fe1f4cf
zhangjianyun <zhangjianyun_kd@163.com>
2023-10-10 15:45:58 +0800

    完成 order和payment服务注册到eureka

--- order80/src/main/java/com/legendyun/order/Order80.java
+++ order80/src/main/java/com/legendyun/order/Order80.java
@@ -2,15 +2,17 @@ package com.legendyun.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * @title: Order8101
+ * @title: Order80
  * @description: TODO
  * @auther: zhangjianyun
  * @date: 2023/10/8 15:01
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Order80 {
 
     public static void main(String[] args) {
--- payment8001/src/main/java/com/legendyun/payment/Payment8001.java
+++ payment8001/src/main/java/com/legendyun/payment/Payment8001.java
@@ -2,6 +2,7 @@ package com.legendyun.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @title: Payment8001
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001 {
 
     public static void main(String[] args) {
