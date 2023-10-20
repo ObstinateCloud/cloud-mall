309c0a547eb383ba69e394caeace6bc176d494a6
zhangjianyun <zhangjianyun_kd@163.com>
2023-10-20 17:27:46 +0800

    application.yml文件拆分

--- payment8001/src/main/java/com/legendyun/payment/Payment8001.java
+++ payment8001/src/main/java/com/legendyun/payment/Payment8001.java
@@ -2,6 +2,7 @@ package com.legendyun.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -12,7 +13,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient  //eureka 专用
+@EnableDiscoveryClient //eureka zookeeper consoul nacoe通用服务注册发现
 public class Payment8001 {
 
     public static void main(String[] args) {
